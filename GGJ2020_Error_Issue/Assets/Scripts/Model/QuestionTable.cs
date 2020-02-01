fd29babcfc434d2eba7e17d2d66f8c990c5a12cb
Takuma Seimiya <mj.soccer.t.s0802@gamil.com>
2020-02-01 13:57:49 +0900

    QuestionのScriptableObject作成

---
+++ GGJ2020_Error_Issue/Assets/Scripts/Model/QuestionTable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(
+    fileName = "CreateQuestionList",
+    menuName = "ScriptableObject/CreateQuestionList")]
+
+public class QuestionTable : ScriptableObject
+{
+  public Answer Answer;
+  public List<Question> Questions = new List<Question>();
+}
+
+[System.Serializable]
+public class Answer
+{
+  public List<string> Answers;
+}
+
+//System.Serializableを設定しないと、データを保持できない(シリアライズできない)ので注意
+[System.Serializable]
+public class Question
+{
+  public string Qustion;
+  public List<int> Answer;
+}
