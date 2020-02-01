25342b0053e7c11dc0e6976591b47ce834790815
taxi-taxi-taxi <takumitakahashi123@gmail.com>
2020-02-02 06:25:57 +0900

    Effect作成

---
+++ GGJ2020_Error_Issue/Assets/Scripts/Effect/Scripts/ShakeView.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShakeView : MonoBehaviour
+{
+   public CameraShake shake;
+
+    public void Shake()
+    {
+        shake.Shake( 9f, 9f); 
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Shake();
+        }
+    }
+}
